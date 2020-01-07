081f3f556e2e1b7ff29ce6d6f72772199edc049a
Ajay <asanivarapu@primetgi.com>
2020-01-07 18:58:13 +0530

    Added changes

--- src/app/components/accounting/accounting-routing.module.ts
+++ src/app/components/accounting/accounting-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TransactionsComponent } from './transactions/transactions.component';
 import { ReportsComponent } from './reports/reports.component';
+import { ProfitComponent } from './reports/profit/profit.component';
 
 
 const routes: Routes = [
-  {path: 'transactions', component: TransactionsComponent},
-  {path: 'reports', component: ReportsComponent},
+  { path: 'transactions', component: TransactionsComponent },
+  {
+    path: 'reports', component: ReportsComponent, children: [
+      {path: 'profit-and-loss', component: ProfitComponent}
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/components/accounting/accounting.module.ts
+++ src/app/components/accounting/accounting.module.ts
@@ -6,13 +6,17 @@ import { TransactionsComponent } from './transactions/transactions.component';
 import { SharedModule } from '../shared/shared.module';
 import { AdddialogComponent } from './transactions/adddialog/adddialog.component';
 import { ReportsComponent } from './reports/reports.component';
+import { NavHeaderComponent } from './reports/nav-header/nav-header.component';
+import { ProfitComponent } from './reports/profit/profit.component';
 
 
 @NgModule({
   declarations: [
     TransactionsComponent,
     AdddialogComponent,
-    ReportsComponent
+    ReportsComponent,
+    NavHeaderComponent,
+    ProfitComponent
   ],
   imports: [
     CommonModule,
