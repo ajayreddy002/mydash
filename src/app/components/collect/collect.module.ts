3ef3ffbff50a8e2801786743a43cb50ac70ad4be
Ajay <asanivarapu@primetgi.com>
2020-02-10 19:31:52 +0530

    Changes

--- src/app/components/collect/collect.module.ts
+++ src/app/components/collect/collect.module.ts
@@ -7,16 +7,18 @@ import { RecievablesComponent } from './recievables/recievables.component';
 import { SharedModule } from '../shared/shared.module';
 import { QuickcollectComponent } from './quickcollect/quickcollect.component';
 import { BulkcollectsComponent } from './bulkcollects/bulkcollects.component';
-
-
+import {DropdownModule} from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     InvoicesComponent,
     RecievablesComponent,
     QuickcollectComponent,
-    BulkcollectsComponent
+    BulkcollectsComponent,
   ],
   imports: [
+    DropdownModule,
+    FormsModule,
     CommonModule,
     CollectRoutingModule,
     SharedModule
--- src/app/components/collect/recievables/recievables.component.ts
+++ src/app/components/collect/recievables/recievables.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recievables.component.scss']
 })
 export class RecievablesComponent implements OnInit {
-
-  constructor() { }
+  customerData: any = [
+    {
+      customer_name: 'ro_test', total_receivables: '₹97,810.00',
+      total_received: '₹0.00', out_standing: '₹97,810.00', dso: '30'
+    },
+    {
+      customer_name: 'Test', total_receivables: '₹97,810.00',
+      total_received: '₹0.00', out_standing: '₹97,810.00', dso: '30'
+    },
+  ];
+  dropDownItems: any[];
+  selectedItem: any;
+  cols: any[];
+  constructor() {
+    this.dropDownItems = [
+      { label: 'FY-2020', value: 'FY' },
+      { label: 'Last Month', value: 'LM' },
+      { label: 'Last 3 Month', value: 'L3M' },
+      { label: 'Last 6 Monhts', value: 'L6M' }
+    ];
+  }
 
   ngOnInit() {
+    this.cols = [
+      { field: 'customer_name', header: 'Customer Name' },
+      { field: 'total_receivables', header: 'Total Receivables' },
+      { field: 'total_received', header: 'Total Received' },
+      { field: 'out_standing', header: 'Outstanding' },
+      { field: 'dso', header: 'DSO' }
+    ];
+    this.selectedItem = 'FY-2020';
   }
 
 }
